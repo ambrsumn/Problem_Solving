298ab224670a3d52050c90d868e269daa4536fae
Amber Suman <90973656+beingambrsumn@users.noreply.github.com>
2022-03-24 12:46:24 +0530

    Added solution - LeetHub

--- Print all nodes that don't have sibling - GFG/print-all-nodes-that-dont-have-sibling.cpp
+++ Print all nodes that don't have sibling - GFG/print-all-nodes-that-dont-have-sibling.cpp
@@ -113,43 +113,26 @@ int main()
 }; */
 
 vector<int> vec;
-void printSingles(Node *root)
+void traverse(Node* root)
 {
-    // Base case
-    if (root == NULL)
-      return;
- 
-    // If this is an internal node, recur for left
-    // and right subtrees
-    if (root->left != NULL && root->right != NULL)
-    {
-        printSingles(root->left);
-        printSingles(root->right);
-    }
- 
-    // If left child is NULL and right is not,
-    // print right child
-    // and recur for right child
-    else if (root->right != NULL)
-    {
-        vec.push_back(root->right->data);
-        printSingles(root->right);
-    }
- 
-    // If right child is NULL and left is
-    // not, print left child
-    // and recur for left child
-    else if (root->left != NULL)
-    {
-vec.push_back(root->left->data);
-        printSingles(root->left);
-    }
+    if(root == NULL)
+    return;
+    
+    if(!root->left && root->right)
+    vec.push_back(root->right->data);
+    
+    if(!root->right && root->left)
+    vec.push_back(root->left->data);
+    
+    traverse(root->left);
+    traverse(root->right);
+    
 }
 
 vector<int> noSibling(Node* node)
 {
     vec.clear();
-    printSingles(node);
+    traverse(node);
     
     if(vec.empty())
     vec.push_back(-1);

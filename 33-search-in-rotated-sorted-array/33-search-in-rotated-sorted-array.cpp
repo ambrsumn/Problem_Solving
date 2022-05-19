73209835299ff087e3723b98fe756ea7f7025fc2
Amber Suman <90973656+beingambrsumn@users.noreply.github.com>
2022-05-19 13:24:03 +0530

    Time: 14 ms (6.37%), Space: 11 MB (95.89%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,92 +1,37 @@
 class Solution {
 public:
-    
-int mid_pos(vector<int> arr, int n)
-{
-    int key = arr[0];
-    int ans=-1;
-
-    int low=0, high=(n-1);
-
-    while(low <= high)
+    int search(vector<int>& nums, int target) 
     {
-        int mid = low + (high-low)/2;
-
-        if(arr[mid] >= key)
-        {
-            key = arr[mid];
-            low = mid+1;
-        }
-
-        else
-        {
-            key = arr[mid];
-            ans = mid;
-            high = mid-1;
-        }
-    }
-
-    if(ans == -1)
-    ans = 0;
-
-    return ans;
-}
-    
-    
-    int binary_search(vector<int> arr, int loww, int highh, int target)
-    {
-        int low = loww, high = highh;
+        int n = nums.size();
+        int  low = 0;
+        int high = n - 1;
         
-        cout<<low<<" "<<high<<endl;
-        int ans=-1;
         
         while(low <= high)
         {
-            int mid = low + (high-low)/2;
+            int mid = (low + high)/2;
             
-            if(arr[mid] == target)
+            if(nums[mid]==target)
+                return mid;
+            
+            if(nums[low] <= nums[mid]) // left half sorted
             {
-                ans = mid;
-                break;
+                if(target < nums[mid] && target >= nums[low])
+                {
+                    high = mid - 1;
+                }
+                else
+                    low = mid + 1;
             }
             
-            else if(arr[mid] < target)
-                low = mid+1;
-            
-            else
-                high = mid-1;
-        }
-        
-        cout<<"ans i s"<<ans<<endl;
-        return ans;
-    }
-    
-    
-    int search(vector<int>& arr, int target) 
-    {
-        int n = arr.size();
-        
-        int indx = mid_pos(arr, n);
-        
-        
-        cout<<indx<<endl;
-        
-        if(arr[indx] == target)
-            return indx;
-        
-        if(indx == 0)
-            return binary_search(arr,0, n-1, target);
-        
-        else
-        {
-            int next = (indx+1)%n;
-            int prev = (indx+n-1)%n;
-            
-            if(target >= arr[0])
-                return binary_search(arr, 0, prev, target);
-            
-            else
-                return binary_search(arr, next, (n-1), target);
+             else // right half sorted
+            {
+                if(target > nums[mid] && nums[high] >= target)
+                {
+                    low = mid + 1;
+                }
+                else high = mid - 1;
+            }
         }
         
         return -1;

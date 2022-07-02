7029bc6212a059fab32da9b732312ea7baf6a7a9
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2022-07-02 09:26:43 +0530

    Time: 19 ms (95.16%), Space: 13.8 MB (60.00%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    
+    bool dfs(vector<vector<int>>& adj, int i, vector<bool> &visited, vector<bool> &vis)
+    {
+        visited[i] = true;
+        vis[i] = true;
+        
+        for(auto it : adj[i])
+        {
+            if(!visited[it])
+            {if(dfs(adj, it, visited, vis)) return true;}
+            
+            else if(vis[it])
+            {
+                // cout<<"YES"<<endl;
+                return true;
+            }
+        }
+        
+        vis[i] = false;
+        return false;
+    }
+    
+    
+    bool canFinish(int n, vector<vector<int>>& pre) 
+    {
+        vector<vector<int>> grid(n);
+        
+        vector<bool> visited(n, false);
+        vector<bool> vis(n, false);
+        
+        for(int i=0; i<pre.size(); i++)
+        {
+            grid[pre[i][0]].push_back(pre[i][1]);
+        }
+        
+        // for(int i=0; i<n; i++)
+        // {
+        //     cout<<i<<" : ";
+        //     for(auto it : grid[i])
+        //     {
+        //         cout<<it<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
+        for(int i=0; i<n; i++)
+        {
+            if(!visited[i])
+                if(dfs(grid, i, visited, vis)) return false;
+        }
+        
+        return true;
+    }
+};

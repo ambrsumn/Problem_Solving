7c13fe1719e4a1aa234bc9dd5377e60c99ca39e0
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2022-07-07 08:09:38 +0530

    Added solution - LeetHub

---
+++ Transitive closure of a Graph - GFG/transitive-closure-of-a-graph.cpp
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// Back-end complete function Template for C++
+
+class Solution{
+public:
+    vector<vector<int>> transitiveClosure(int n, vector<vector<int>> graph)
+    {
+        vector<vector<int>> reach(n, vector<int>(n, 0));
+        
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                reach[i][j] = graph[i][j];
+            }
+        }
+        
+        for(int k=0; k<n; k++)
+        {
+            for(int i=0; i<n; i++)
+            {
+                for(int j=0; j<n; j++)
+                {
+                    if(i == j)
+                    reach[i][j] = 1;
+                    
+                    else
+                    reach[i][j] = (reach[i][j] || (reach[i][k] && reach[k][j]));
+                }
+            }
+        }
+        
+        return reach;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        vector<vector<int>> graph(N, vector<int>(N, -1));
+        for(int i = 0;i < N;i++)
+            for(int j=0;j<N;j++)
+            cin>>graph[i][j];
+        
+        Solution ob;
+        vector<vector<int>> ans = ob.transitiveClosure(N, graph);
+        for(int i = 0;i < N;i++)
+            {for(int j = 0;j < N;j++)
+                cout<<ans[i][j]<<" ";
+        cout<<"\n";}
+    }
+    return 0;
+}  // } Driver Code Ends

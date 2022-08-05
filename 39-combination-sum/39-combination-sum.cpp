36754f8ff9223930d1c458c43f698ebc99fdd7aa
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2022-08-05 20:51:45 +0530

    Time: 16 ms (50.61%), Space: 11.3 MB (53.82%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    vector<int> temp;
+    vector<vector<int>> ans;
+    
+    void bt(int i, vector<int>& vec, int &tar, int sum)
+    {   
+        if(sum == tar)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        
+        if(sum>tar)
+            return;
+        
+        while(i<vec.size())
+        {
+            temp.push_back(vec[i]);
+            bt(i,vec,tar,sum+vec[i]);
+            i++;
+            temp.pop_back();
+        }
+        
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& vec, int tar) 
+    {
+        sort(vec.begin(), vec.end());
+        vec.erase(unique(vec.begin(),vec.end()),vec.end());
+        
+        bt(0,vec,tar,0);
+        
+        return ans;
+    }
+};

012f5a3a2df609cf128f2cb34c0c541b5d227d6e
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2022-08-23 00:04:12 +0530

    Time: 10 ms (46.61%), Space: 6.3 MB (52.81%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
+    
+  vector<int> vec;
+  
+  int recur(int i, int &k)
+  {
+      if(vec.size()==1)return vec[0];
+      
+      i = (i+k)%vec.size();
+      vec.erase(vec.begin()+i);
+      
+      return recur(i,k);
+  }
+    
     int findTheWinner(int n, int k) 
     {
-        deque<int> dq;
         for(int i=1; i<=n; i++)
-            dq.push_back(i);
+            vec.push_back(i);
         
-    
-    while(dq.size() != 1)
-    {
-        int count=1;
-        while(count<k)
-        {
-            int d = dq.front();
-            dq.pop_front();
-            dq.push_back(d);
-            
-            count++;
-        }
-        // cout<<dq.front()<<endl;
-        dq.pop_front();
-    }
-    
-    return dq.front();
+        k--;
+        
+    return recur(0,k);
     }
 };

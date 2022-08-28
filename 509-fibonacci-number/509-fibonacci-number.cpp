0f7ed0bec1ed52caebe55b197c94a5c58dd91ee7
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2022-08-28 15:33:13 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (8.67%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    int recur(int n, vector<int> &dp)
+    {
+        if(n <= 1)return n;
+        
+        if(dp[n] != -1)return dp[n];
+        
+        return dp[n] = recur(n-1,dp) + recur(n-2,dp);
+    }
+    
+    int fib(int n) 
+    {
+        vector<int> dp(n+1, -1);
+        
+        /*
+        
+        TABULATION METHOD :-
+        
+        dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=2; i<=n; i++)
+        {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
+        
+        */
+        
+        return recur(n, dp);
+    }
+};

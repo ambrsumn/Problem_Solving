b804d55ad586bf844154437fbd4484eab7c96a06
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2022-08-28 15:30:42 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (16.13%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -14,6 +14,18 @@ public:
     {
         vector<int> dp(n+1, -1);
         
-        return recur(n, dp);
+        dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=2; i<=n; i++)
+        {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
+        
+       
+        
+        // return recur(n, dp);
     }
 };

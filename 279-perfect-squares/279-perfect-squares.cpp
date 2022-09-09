6fc919db93a59c50d3c366910dd1b4ab32278eb1
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2022-09-09 17:32:10 +0530

    Time: 1355 ms (5.00%), Space: 259.4 MB (5.73%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    int recur(int i, int n, vector<vector<int>> &dp)
+    {
+        if(n<0)return 1e8;
+        if(n==0)return 0;
+        if((i*i)>n)return 1e8;
+        
+        if(dp[i][n] != -1)return dp[i][n];
+        
+        //PICK - NOT_PICK
+        
+        int pick = 1+recur(i, (n-(i*i)), dp);
+        int np = recur(i+1, n, dp);
+        
+        return dp[i][n] = min(pick, np);
+        
+    }
+    
+    int numSquares(int n) 
+    {
+        vector<vector<int>> dp(101, vector<int>(n+1, -1));
+        return recur(1, n, dp);
+    }
+};

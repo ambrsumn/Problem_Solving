4d44adc7c27606321feb9a9821637e97c6bf5bdd
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2022-09-19 17:34:17 +0530

    Time: 3 ms (89.48%), Space: 9.7 MB (56.21%) - LeetHub

---
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    int recur(vector<int>& days, vector<int>& costs, int i, vector<int> &dp)
+    {
+        // cout<<i<<endl;
+        if(i >= days.size())return 0;
+        
+        if(dp[i] != -1)return dp[i];
+        
+        int a=0, b=0, c=0;
+        
+        a = costs[0] + recur(days, costs, i+1, dp);
+        
+        int idx = lower_bound(days.begin(), days.end(), days[i]+7)-days.begin();
+        
+        b = costs[1] + recur(days, costs, idx, dp);
+        
+        int indx = lower_bound(days.begin(), days.end(), days[i]+30)-days.begin();
+        
+        c = costs[2] + recur(days, costs, indx, dp);
+        
+        return dp[i] = min(a, min(b, c));
+    }
+    
+    int mincostTickets(vector<int>& days, vector<int>& costs) 
+    {
+        vector<int> dp(days.size()+1, -1);
+        
+        return recur(days, costs, 0, dp);
+    }
+};

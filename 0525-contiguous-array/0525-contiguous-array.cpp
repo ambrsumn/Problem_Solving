2801b03966fdf87fa6f2045137d0306b8f88f353
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2022-10-11 21:37:36 +0530

    Time: 344 ms (10.62%), Space: 84.2 MB (22.59%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -4,9 +4,6 @@ public:
     {
         int ans = 0;
         
-        for(int i=nums.size()-1; i>=0; i--)
-            if(nums[i] == 0)nums[i]=-1;
-        
         map<int,int> mpp;
         
         int sum=0;
@@ -15,7 +12,7 @@ public:
         
         for(int i=0; i<nums.size(); i++)
         {
-            sum += nums[i];
+            sum += nums[i] == 0 ? -1 : 1;
             
             if(mpp.count(sum))
                 ans = max(ans, i-mpp[sum]);

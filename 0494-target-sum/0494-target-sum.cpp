4fb21f5f1457d919fa9bfa5d085feef17254533c
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2023-04-17 19:30:39 +0530

    Time: 339 ms (35.99%), Space: 62.6 MB (8.12%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     
     int target=0;
+    map<pair<int,int>, int> mpp;
     
     int recur(int i, vector<int> &nums, int curr)
     {
-        // cout<<i<<" "<<curr<<endl;
         if(i>=nums.size())
         {
             if(curr == target)return 1;
             return 0;
         }
         
+        if(mpp.count({i, curr}) != 0)return mpp[{i, curr}];
+        
         int plus = recur(i+1, nums, curr+nums[i]);
         int minus = recur(i+1, nums, curr-nums[i]);
         
-        return plus+minus;
+        return mpp[{i, curr}] = plus+minus;
     }
     
     int findTargetSumWays(vector<int>& nums, int t) 

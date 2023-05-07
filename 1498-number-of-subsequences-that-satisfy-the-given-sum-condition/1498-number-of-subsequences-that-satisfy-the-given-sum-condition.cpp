9115dbf76747f88be6cfd45559a0a3ecdf291d84
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2023-05-08 00:34:26 +0530

    Time: 210 ms (8.64%), Space: 47.8 MB (85.11%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,33 +1,32 @@
-class Solution 
-{
+class Solution {
 public:
-    int mod = 1e9 + 7;
-    int binExp(int a, int b)
-    {
-        int ans = 1;
-        while(b)
-        {
-            if(b % 2 != 0)
-            {
-                ans = (a * 1LL* ans) % mod;
-            }
+
+    int MOD = 1e9 + 7;
+
+    int powermod(int a, int b){
+        int res = 1;
+        while(b){
+            if(b & 1) res = 1ll * res * a % MOD;
             b /= 2;
-            a = (a * 1LL * a) % mod;
+            a = 1ll * a * a % MOD;
         }
-        return ans;
+        return res;
     }
-    int numSubseq(vector<int>& nums, int target) 
-    {
+
+    int numSubseq(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        int ans = 0;
+
         sort(nums.begin(), nums.end());
-        int cnt = 0, n = nums.size();
-        for(int i = 0; i < n; i++)
-        {
-            int pos = upper_bound(nums.begin(), nums.end(), target - nums[i]) - nums.begin() - 1;
-            if(pos >= i)
-            {
-                cnt = (cnt % mod + binExp(2, pos - i) % mod) % mod;
-            }
+
+        for(int i = 0; i < n; i++){
+            int ub = upper_bound(nums.begin(), nums.end(), target - nums[i]) - nums.begin();
+            if(ub <= i) break;
+            ans = (ans + powermod(2, ub - i - 1)) % MOD;
         }
-        return cnt % mod;
+
+        return ans;
+
     }
 };

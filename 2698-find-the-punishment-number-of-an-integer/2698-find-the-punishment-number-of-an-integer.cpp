88787fba728f705c3202e5e4ce37a43de420c396
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2023-05-26 03:20:22 +0530

    Time: 298 ms (50.62%), Space: 5.9 MB (95.65%) - LeetHub

---
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+    int chk=0;
+    bool flag = false;
+    
+void recur(int i, string &t, int sum)
+{
+	if (i >= t.size())
+	{
+		if(sum == chk)flag=true;
+		return;
+	}
+
+	for (int j = 1; j <= t.length() - i; j++)
+	{
+		int d = stoi(t.substr(i, j));
+
+		recur(i + j, t, sum + d);
+	}
+
+	return;
+}
+    
+    int punishmentNumber(int n) 
+    {
+        int ans = 0;
+        for(int i=1; i<=n; i++)
+        {
+            int temp = pow(i,2);
+            string t = to_string(temp);
+            
+            chk = i;
+            flag=false;
+            recur(0, t, 0);
+            
+            if(flag)ans += temp;
+        }
+        
+        return ans;
+    }
+};

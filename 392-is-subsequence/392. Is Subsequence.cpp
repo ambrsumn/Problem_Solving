b0039c90769ce8033f161483db015aae8112d1cf
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2025-02-08 03:37:42 +0530

    Runtime: 107 ms (2.98%), Memory: 283.1 MB (5.14%) - Gitcode

--- 392-is-subsequence/392. Is Subsequence.cpp
+++ 392-is-subsequence/392. Is Subsequence.cpp
@@ -1,43 +1,18 @@
 class Solution {
 public:
 
-// bool recur(int i, int j, string s, string t) {
+bool recur(int i, int j, string s, string t) {
 
-//     if(i >= s.length() && j <= t.size())return true;
-//     if(j >= t.length())return false;
+    if(i >= s.length() && j <= t.size())return true;
+    if(j >= t.length())return false;
+    // if(i<s.length()-1 && j>=t.length())return false;
 
-//     if(s[i] == t[j])return recur(i+1, j+1, s, t);
-//     else return recur(i, j+1, s, t);
-// }
+    if(s[i] == t[j])return recur(i+1, j+1, s, t);
+    else return recur(i, j+1, s, t);
+}
 
     bool isSubsequence(string s, string t) {
 
-        // return recur(0, 0, s, t);
-
-        queue<char> sub;
-        queue<char> str;
-
-        for(auto it : s)sub.push(it);
-
-        for(auto it : t)str.push(it);
-
-        while(!str.empty() && !sub.empty())
-        {
-            while(!str.empty() && str.front() != sub.front())
-            {
-                str.pop();
-            }
-
-            if(str.empty() && !sub.empty())return false;
-
-            if(sub.empty())return true;
-
-            str.pop();
-            sub.pop();
-
-        }
-            if(sub.empty())return true;
-
-            return false;
+        return recur(0, 0, s, t);
     }
 };

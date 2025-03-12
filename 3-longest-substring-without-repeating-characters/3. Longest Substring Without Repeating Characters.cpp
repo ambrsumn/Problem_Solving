a8a9d6ff114c4d3027937694dff0ca70af837350
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2025-03-12 21:24:49 +0530

    Runtime: 387 ms (7.20%), Memory: 17 MB (9.17%) - Gitcode

---
+++ 3-longest-substring-without-repeating-characters/3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        map<char, int> mpp;
+
+        int i=0, j=0, ans = 0;
+        while(j < s.length())
+        {
+            if(mpp.find(s[j]) == mpp.end())
+            {
+                ans = max(ans, (j-i+1));
+                cout<<j<<" "<<i<<" "<<ans<<endl;
+                mpp[s[j]]++;
+            }
+            else
+            {
+                cout<<"going in ";
+                while(i<=j)
+                {
+                    if(s[i] == s[j])
+                    {
+                        mpp.erase(s[i]);
+                        i++;
+                        break;                        
+                    }
+                    else
+                    {
+                        mpp.erase(s[i]);
+                        i++;
+                    }
+                }
+                mpp[s[j]]++;
+
+                cout<<i<<endl;
+            }
+
+            j++;
+        }
+
+        return ans;
+    }
+};

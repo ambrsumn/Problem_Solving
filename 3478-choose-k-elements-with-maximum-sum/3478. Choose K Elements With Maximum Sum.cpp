cd9e5013503d0d63987b32241fad5489c9925754
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2025-03-14 22:34:40 +0530

    Runtime: 98 ms (67.18%), Memory: 180.2 MB (40.64%) - Gitcode

---
+++ 3478-choose-k-elements-with-maximum-sum/3478. Choose K Elements With Maximum Sum.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    vector<long long> findMaxSum(vector<int>& nums1, vector<int>& nums2, int k) {
+
+        int n = nums1.size();
+        vector<long long> ans(n, 0);
+        vector<pair<int,int>> vec;
+
+        for(int i=0; i<n; i++)vec.push_back({nums1[i], i});
+        sort(vec.begin(), vec.end());
+        priority_queue<int, vector<int>, greater<int>> pq;
+        vector<int> left;
+        long long currentSum = 0;
+
+        for(int i=0; i<n; i++)
+        {
+            if(i != 0)
+            {
+                if(vec[i-1].first != vec[i].first) 
+                {
+                    for(auto it : left)
+                    {
+                        pq.push(nums2[it]);
+                        // left.pop();
+                        currentSum += nums2[it];
+
+                        if(pq.size() > k)
+                        {
+                            currentSum -= pq.top();
+                            pq.pop();
+                        }
+                    }
+                    left.clear();
+                    
+                    pq.push(nums2[vec[i-1].second]);
+                    
+                    currentSum += nums2[vec[i-1].second];
+
+                    if(pq.size() > k)
+                    {
+                        currentSum -= pq.top();
+                        pq.pop();
+                    }
+
+                }
+                else left.push_back(vec[i-1].second);
+
+
+            }
+
+            // priority_queue<int, vector<int>, greater<int>> temp = pq;
+
+            long long indexSum = 0;
+
+            // while(!temp.empty())
+            // {
+            //     int top = temp.top();
+            //     indexSum += top;
+
+            //     temp.pop();
+            // }
+
+            ans[vec[i].second] = currentSum;
+
+        }
+
+        return ans;
+    }
+};

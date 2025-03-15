798adda3b7870486b00903c5d5b68d7ca31edcef
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2025-03-15 14:25:20 +0530

    Runtime: 35 ms (20.78%), Memory: 63.5 MB (15.75%) - Gitcode

--- 2560-house-robber-iv/2560. House Robber IV.cpp
+++ 2560-house-robber-iv/2560. House Robber IV.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    
-    bool poss(int mid, vector<int> &nums, int k)
+
+    bool isPossible(int mid, vector<int> &nums, int k)
     {
         for(int i=0; i<nums.size(); i++)
         {
@@ -16,27 +16,28 @@ public:
         
         return false;
     }
-    
-    int minCapability(vector<int>& nums, int k) 
-    {
-        int low = 0;
-        int high = 1e9;
-        int ans=1e7;
-        
+    int minCapability(vector<int>& nums, int k) {
+
+        vector<int> temp = nums;
+        sort(temp.begin(), temp.end());
+        int ans = INT_MAX;
+        int low = temp[0], high = temp.back();
+
+        cout<<low<<" "<<high<<endl;
+
         while(low <= high)
         {
-            int mid = low + (high-low)/2;
-            
-            if(poss(mid, nums, k))
+            int mid = low + (high - low)/2;
+
+            if(isPossible(mid, nums, k)) 
             {
                 ans = mid;
                 high = mid-1;
             }
-            
-            else
-                low = mid+1; 
+            else low = mid+1;
         }
-        
+
         return ans;
+        
     }
 };

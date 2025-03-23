d884869046b1f31c4cfac044b5d58532416179a9
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2025-03-23 15:39:45 +0530

    Runtime: 0 ms (100.00%), Memory: 19.7 MB (60.42%) - Gitcode

--- 547-number-of-provinces/547. Number of Provinces.cpp
+++ 547-number-of-provinces/547. Number of Provinces.cpp
@@ -1,26 +1,9 @@
 class Solution {
 public:
-
-    void recur(int i, vector<vector<int>>& isConnected, vector<int> &visited, int &n)
-    {
-        if(i >= n)return;
-
-        for(int j=0; j<n; j++)
-        {
-            if(j != i && isConnected[i][j] == 1 && visited[j] == 0)
-            {
-                visited[j] = 1;
-                recur(j, isConnected, visited, n);
-            }
-        }
-
-        return;
-    }
-
     int findCircleNum(vector<vector<int>>& isConnected) {
-        
-        vector<int> visited(isConnected.size(), 0);
+        queue<int> graph;
         int n = isConnected.size();
+        vector<int> visited(n, 0);
         int ans = 0;
 
         for(int i=0; i<n; i++)
@@ -28,8 +11,23 @@ public:
             if(visited[i] == 0)
             {
                 visited[i] = 1;
-                recur(i, isConnected, visited, n);
-                ans++;
+                graph.push(i);
+                ans ++;
+            }
+
+            while(!graph.empty())
+            {
+                int curr = graph.front();
+                graph.pop();
+
+                for(int j=0; j<n; j++)
+                {
+                    if(isConnected[curr][j] == 1 && visited[j] == 0 && curr != j)
+                    {
+                        visited[j] = 1;
+                        graph.push(j);
+                    }
+                }
             }
         }
 

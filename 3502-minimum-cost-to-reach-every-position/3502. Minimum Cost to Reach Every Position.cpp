61c0e25bc86ed2ae20bb4f4bc9a7ec7031ad3aa6
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2025-03-31 16:34:56 +0530

    Runtime: 0 ms (100.00%), Memory: 26.6 MB (60.87%) - Gitcode

--- 3502-minimum-cost-to-reach-every-position/3502. Minimum Cost to Reach Every Position.cpp
+++ 3502-minimum-cost-to-reach-every-position/3502. Minimum Cost to Reach Every Position.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     vector<int> minCosts(vector<int>& cost) {
-
-        vector<int> ans;
-
-        for(auto it : cost)ans.push_back(it);
         int mini = INT_MAX;
 
         for(int i=0; i<cost.size(); i++)
         {
             mini = min(mini, cost[i]);
-            ans[i] = mini;
+            cost[i] = mini;
         }
 
-        return ans;
+        return cost;
         
     }
 };

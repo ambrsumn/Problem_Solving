b91002e4ea2b4aa8172ef6a87a5c8a264a309631
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2025-04-08 13:24:15 +0530

    Runtime: 251 ms (7.69%), Memory: 30.7 MB (52.24%) - Gitcode

--- 416-partition-equal-subset-sum/416. Partition Equal Subset Sum.cpp
+++ 416-partition-equal-subset-sum/416. Partition Equal Subset Sum.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    int sum=0;
-    
-    bool bt(int i, vector<int>& nums, int temp, vector<vector<int>> &dp)
+
+    int req=0;
+    int dp[201][20001];
+
+    bool recur(int i, int curr, vector<int>& nums)
     {
-        if(temp == (sum-temp))return true;
-        if(i>=nums.size() || temp>(sum-temp))return false;
-        
-            if(dp[i][temp] != -1)return dp[i][temp];
-        
-        return dp[i][temp] = bt(i+1,nums,temp+nums[i],dp) | bt(i+1,nums,temp,dp);
+        if(i >= nums.size())return curr == req;
+        if(dp[i][curr] != -1)return dp[i][curr];
+
+        bool pick = recur(i+1, curr+nums[i], nums);
+        bool np = recur(i+1, curr, nums);
+
+        return dp[i][curr] = pick | np;
     }
-    
-    bool canPartition(vector<int>& nums) 
-    {   
-        for(auto it : nums)
-            sum += it;
-        
-        // sort(nums.begin(), nums.end());
-        int n = nums.size();
-        
-        vector<vector<int>> dp(n, vector<int>(sum+1, -1));
-        
-        return bt(0,nums,0,dp);
+
+    bool canPartition(vector<int>& nums) {
+
+        int totalSum=0;
+        memset(dp, -1, sizeof(dp));
+
+        for(auto it : nums)totalSum += it;
+
+        if(totalSum%2 != 0)return false;
+
+        int i=0, target = totalSum/2;
+        req = target;
+
+        return recur(i, 0, nums);
     }
 };

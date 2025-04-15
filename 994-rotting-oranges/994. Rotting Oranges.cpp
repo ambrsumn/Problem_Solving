dbdd559dbc3112da2b101959022079279a05271c
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2025-04-15 11:50:39 +0530

    Runtime: 0 ms (100.00%), Memory: 16.8 MB (56.91%) - Gitcode

---
+++ 994-rotting-oranges/994. Rotting Oranges.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+
+        int n = grid.size(), m = grid[0].size(), ans = 0, total=0, rotten=0;
+
+        queue<pair<int,int>> q;
+        // vector<vector<int>> visited(n, vector<int>(m, -1));
+
+
+        // AT TIME 0
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(grid[i][j] == 2)q.push({i,j});
+                if(grid[i][j] == 2 || grid[i][j] == 1)total++;
+            }
+        }
+
+        while(!q.empty())
+        {
+            int currentSize = q.size(); 
+            rotten += currentSize;
+
+            while(currentSize--)
+            {
+                pair<int,int> top = q.front();
+                q.pop();
+
+                int i=top.first, j=top.second;
+
+                if(i-1 >= 0 && grid[i-1][j] == 1)
+                {
+                    grid[i-1][j] = 2;
+                    q.push({i-1, j});
+                }
+
+                if(i+1 < n && grid[i+1][j] == 1)
+                {
+                    grid[i+1][j] = 2;
+                    q.push({i+1, j});
+                }
+                if(j-1 >= 0 && grid[i][j-1] == 1)
+                {
+                    grid[i][j-1] = 2;
+                    q.push({i, j-1});
+                }
+                if(j+1 < m && grid[i][j+1] == 1)
+                {
+                    grid[i][j+1] = 2;
+                    q.push({i, j+1});
+                }
+            }
+
+            if(q.size() > 0)ans++;
+        }
+
+        return total==rotten? ans : -1; 
+    }
+};

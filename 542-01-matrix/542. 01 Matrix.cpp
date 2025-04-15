48612af733b17f1ee9c75121c0a47abc50d59048
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2025-04-15 17:12:23 +0530

    Runtime: 55 ms (12.80%), Memory: 36.5 MB (59.69%) - Gitcode

---
+++ 542-01-matrix/542. 01 Matrix.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+
+        int n = mat.size(), m = mat[0].size(), dis = 0;
+
+        vector<vector<int>> ans(n, vector<int>(m, -1));
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        queue<pair<int, int>> q;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0) {
+
+                    ans[i][j] = 0;
+                    visited[i][j] = 1;
+
+                    if (i - 1 >= 0 && mat[i - 1][j] == 1 && visited[i-1][j] != 1) { 
+                        q.push({i - 1, j});
+                        visited[i-1][j] = 1;
+                    }
+                    if (j - 1 >= 0 && mat[i][j - 1] == 1 && visited[i][j-1] != 1) {
+
+                        q.push({i, j - 1});
+                        visited[i][j-1] = 1;
+
+                    }
+                    if (j + 1 < m && mat[i][j + 1] == 1 && visited[i][j+1] != 1) {
+
+                        q.push({i, j + 1});
+                        visited[i][j+1] = 1;
+
+                    }
+                    if (i + 1 < n && mat[i + 1][j] == 1 && visited[i+1][j] != 1) {
+                        q.push({i + 1, j});
+                        visited[i+1][j] = 1;
+                    }
+                }
+
+                // cout<<q.size()<<endl;
+            }
+        }
+
+        while (!q.empty()) {
+            dis++;
+            int currSize = q.size();
+            cout << dis << " " << currSize << endl;
+
+            while (currSize--) {
+                int i = q.front().first;
+                int j = q.front().second;
+                q.pop();
+
+                ans[i][j] = dis;
+
+                if (i - 1 >= 0 && mat[i - 1][j] == 1 && visited[i-1][j] != 1) { 
+                    q.push({i - 1, j});
+                    visited[i-1][j] = 1;
+                }
+                if (j - 1 >= 0 && mat[i][j - 1] == 1 && visited[i][j-1] != 1) {
+
+                    q.push({i, j - 1});
+                    visited[i][j-1] = 1;
+
+                }
+                if (j + 1 < m && mat[i][j + 1] == 1 && visited[i][j+1] != 1) {
+
+                    q.push({i, j + 1});
+                    visited[i][j+1] = 1;
+
+                }
+                if (i + 1 < n && mat[i + 1][j] == 1 && visited[i+1][j] != 1) {
+                    q.push({i + 1, j});
+                    visited[i+1][j] = 1;
+                }
+            }
+        }
+
+        return ans;
+    }
+};

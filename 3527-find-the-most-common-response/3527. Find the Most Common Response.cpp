1a018f161d90dba5eb055dbc01e2d26382a5b66f
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2025-05-05 13:30:48 +0530

    Runtime: 2668 ms (5.01%), Memory: 522.3 MB (23.11%) - Gitcode

--- 3527-find-the-most-common-response/3527. Find the Most Common Response.cpp
+++ 3527-find-the-most-common-response/3527. Find the Most Common Response.cpp
@@ -13,17 +13,34 @@ public:
 
             for(auto itt : it)st.insert(itt);
 
-            for(auto itr : st)mpp[itr]++;
-        }
-
-        for(auto it : mpp)
-        {
-            if(it.second > maxi)
+            for(auto itr : st)
             {
-                maxi = it.second;
-                ans = it.first;
+                mpp[itr]++;
+
+                if(mpp[itr] > maxi)
+                {
+                    maxi = mpp[itr];
+                    ans = itr;
+                    cout<<maxi<<endl;
+                }
+
+                else if(mpp[itr] == maxi && ans > itr)
+                {
+                    // maxi = mpp[itr];
+                    ans = itr;
+                    // cout<<maxi<<endl;
+                }
             }
         }
+
+        // for(auto it : mpp)
+        // {
+        //     if(it.second > maxi)
+        //     {
+        //         maxi = it.second;
+        //         ans = it.first;
+        //     }
+        // }
 
         return ans;
         

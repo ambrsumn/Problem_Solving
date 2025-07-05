0c0f87b2c523de0d4a57ed66db6b68567523e5d1
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2025-07-05 23:47:23 +0530

    Runtime: 1975 ms (5.57%), Memory: 44.9 MB (10.67%) - Gitcode

--- 918-maximum-sum-circular-subarray/918. Maximum Sum Circular Subarray.cpp
+++ 918-maximum-sum-circular-subarray/918. Maximum Sum Circular Subarray.cpp
@@ -5,49 +5,38 @@ public:
     {
         int sum=0, lsum=0, rsum=0, maxi=INT_MIN;
         int n = nums.size();
-        // cout<<i<<" "<<lsum<<" "<<rsum<<endl;
 
         int j = (i+1)%n;
 
         //right sum - max possible
         while(j != i)
         {
-            // cout<<"r "<<j<<endl;
-            // cout<<i<<" "<<j<<endl;
-            // cout<<i<<" "<<j<<" ";
             sum += nums[j];
             rsum = max(rsum, sum);
             
             if(sum <= 0)break;
 
             (j+1 >= n) ? j = (j+1)%n : j++; 
-            // cout<
         }
         sum=0;
 
 
-        if(j == i)
-        {
-            // cout<<i<<" "<<rsum<<" \n\n";
-            return max(nums[i] + lsum + rsum, lsum+rsum);
-        }
+        if(j == i)return max(nums[i] + lsum + rsum, lsum+rsum);
 
 
         //left sum - max possible
         j = (i-1+n)%n;
         while(j != i)
         {
-            // cout<<"l "<<j<<endl;
             sum += nums[j];
             lsum = max(lsum, sum);
             
             if(sum <= 0)break;
             
             (j-1 < 0) ? j = (j-1+n)%n : j--; 
-            // j = (j-1+n)%n;
+
         }
 
-        // cout<<i<<" "<<lsum<<" "<<rsum<<endl<<endl;;
 
         return max({nums[i] + lsum + rsum, lsum, rsum, nums[i]});
     }

c60f6b03f78f43e67f1720837a97fba4b8e5bee7
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2025-07-14 19:11:31 +0530

    Runtime: 2306 ms (5.00%), Memory: 552 MB (5.01%) - Gitcode

---
+++ 3613-minimize-maximum-component-cost/3613. Minimize Maximum Component Cost.cpp
@@ -0,0 +1,82 @@
+class DSU {
+private:
+    vector<int> parent;
+    vector<int> rank;
+
+public:
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n);
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    int findUltimateParent(int i) {
+        if (parent[i] == i) return i;
+        return parent[i] = findUltimateParent(parent[i]);  
+    }
+
+    void createDSU(int i, int j) {
+        int parentI = findUltimateParent(i);
+        int parentJ = findUltimateParent(j);
+
+        if (parentI == parentJ) return;
+
+        if (rank[parentJ] > rank[parentI]) 
+        {
+            parent[parentI] = parentJ;
+        } 
+        else if (rank[parentJ] < rank[parentI]) 
+        {
+            parent[parentJ] = parentI;
+        } 
+        else 
+        {
+            parent[parentI] = parentJ;
+            rank[parentJ]++;
+        }
+    }
+
+    int countComponents() {
+        unordered_set<int> counter;
+        for (int i = 0; i < parent.size(); ++i) {
+            counter.insert(findUltimateParent(i));
+        }
+        return counter.size();
+    }
+};
+
+
+class Solution {
+public:
+    int minCost(int n, vector<vector<int>>& edges, int k) {
+
+        int low = 0, high = -1;
+        int ans = 0;
+
+        for(auto it : edges)high = max(high, it[2]);
+
+        while(low <= high)
+        {
+            int mid = low + (high-low)/2;
+
+            DSU newDsu = DSU(n);
+
+            for(auto it : edges)if(it[2] <= mid)newDsu.createDSU(it[0], it[1]);
+
+            int cc = newDsu.countComponents();
+
+            if(cc <= k)
+            {
+                ans = mid;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+
+        return ans;
+        
+    }
+};

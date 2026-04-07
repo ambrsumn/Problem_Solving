41f76e8b05df46f9996ccfeae4c69f208ddea488
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2026-04-07 22:47:04 +0530

    Runtime: 19 ms (14.69%), Memory: 31.4 MB (96.36%) - Gitcode

--- 628-maximum-product-of-three-numbers/628. Maximum Product of Three Numbers.cpp
+++ 628-maximum-product-of-three-numbers/628. Maximum Product of Three Numbers.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
 
-        int neg = 0;
-        int pos = 0;
-
         sort(nums.begin(), nums.end());
         int n = nums.size();
 
@@ -16,12 +13,6 @@ public:
         if(nums[0] >= 0)
         {
             return nums[n-1] * nums[n-2] * nums[n-3];
-        }
-
-        for(auto it : nums)
-        {
-            if(it < 0)neg++;
-            else pos++;
         }
 
         return nums[n-1] * max((nums[0]*nums[1]), (nums[n-2]*nums[n-3]));

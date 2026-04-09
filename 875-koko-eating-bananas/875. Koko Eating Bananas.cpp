0e5c9cf6c4022398958d6eb7db614849c707ce42
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2026-04-09 17:35:18 +0530

    Runtime: 0 ms (100.00%), Memory: 22.8 MB (97.89%) - Gitcode

--- 875-koko-eating-bananas/875. Koko Eating Bananas.cpp
+++ 875-koko-eating-bananas/875. Koko Eating Bananas.cpp
@@ -1,42 +1,46 @@
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& piles, int h) {
-        sort(begin(piles), end(piles));
-        int si = 1;
-        int ei = piles.back();
-        int posi = ei;
-        while (si <= ei) {
-            int m = si + (ei - si) / 2;
-            if (isPossible(piles, h, m)) {
-                posi = m;
-                ei = m - 1;
-            } else {
-                si = m + 1;
-            }
+
+// s = v*t
+// t = s/v
+
+// s = 6
+// v = 2
+
+// t = s/v = 6/2 = 3
+
+    bool canEat(vector<int>& piles, int h, int mid)
+    {
+        // nikalna hai total time to eat all kelas with this speed(mid)
+
+        long long time = 0;
+        for(auto it : piles)
+        {
+            time += ceil((it*1.0)/mid);
         }
-        return posi;
+
+        if(time <= h)return true;
+        return false;
     }
-private:
-    bool isPossible(vector<int> &piles, int h, int curr) {
-        long long cnt = 0;
-        for (int i = 0;i < piles.size();i++) {
-            int p = piles[i];
-            if (p <= curr) {
-                cnt++;
-            } else {
-                long long a = 0;
-                while (p) {
-                    a += (p/curr);
-                    p = (p % curr);
-                    if (p == 0) break;
-                    if (p < curr) {
-                        a++;
-                        break;
-                    }
-                }
-                cnt += a;
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        sort(piles.begin(), piles.end());
+
+        int low = 1;
+        int high = piles.back();
+        int ans = high;
+
+        while(low <= high)
+        {
+            int mid = low + (high - low)/2;
+            if(canEat(piles, h, mid))
+            {
+                ans = min(ans, mid);
+                high = mid-1;
             }
+            else low = mid+1;
         }
-        return (cnt <= h);
+
+        return ans;
     }
 };

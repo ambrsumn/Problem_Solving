341144849cc836082978518636a0a42f506cdf87
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2026-04-14 10:44:25 +0530

    Runtime: 0 ms (100.00%), Memory: 8.4 MB (85.66%) - Gitcode

--- 69-sqrtx/69. Sqrt(x).cpp
+++ 69-sqrtx/69. Sqrt(x).cpp
@@ -2,7 +2,27 @@ class Solution {
 public:
     int mySqrt(int x) {
 
-        return pow(x, 0.5);
-        
+        int low = 0, high = x;
+        int ans;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            long square = (long)mid * (long)mid;
+
+            long diff = x - square;
+
+            if (diff == 0)
+                return mid;
+
+            if (diff > 0) {
+                ans = mid;
+                low = mid + 1;
+            }
+
+            else high = mid - 1;
+        }
+
+        return ans;
     }
 };

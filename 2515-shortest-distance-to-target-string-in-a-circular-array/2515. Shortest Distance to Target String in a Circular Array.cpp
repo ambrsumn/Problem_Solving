a283923c2d11186064b8fce7a7a950f6d48ed0a3
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2026-04-16 20:24:43 +0530

    Runtime: 0 ms (100.00%), Memory: 18.4 MB (9.52%) - Gitcode

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515. Shortest Distance to Target String in a Circular Array.cpp
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515. Shortest Distance to Target String in a Circular Array.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
-    int closestTarget(vector<string>& words, string target, int st) {
-
-        auto it = find(words.begin(), words.end(), target);
-        if(it == words.end())return -1;
-
-        int ans = INT_MAX;
-        int n = words.size();
-
-        int count = 0;
-        int i = st;
-        while(words[i] != target) 
+    int closestTarget(vector<string>& words, string target, int startIndex) 
+    {
+        int n=words.size();
+        for(int i=0;i<n;i++)
         {
-            if (words[i] == target)
-                break;
-            count++;
-            i = (i + 1) % n;
+            if ((words[(startIndex+i)%n]==target) ||
+                (words[(startIndex-i+n)%n]==target))
+            {
+                return i;
+            }
         }
-
-        ans = min(ans, count);
-
-        i = st;
-        count=0;
-
-        while(words[i] != target) 
-        {
-            if (words[i] == target)
-                break;
-            count++;
-            i = (i - 1 + n) % n;
-        }
-
-        ans = min(ans, count);
-
-        return ans;
+        return -1;
     }
 };

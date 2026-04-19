23e2ae4a2dcdd39e6464f2e0b64d155bdc7037dc
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2026-04-19 13:54:31 +0530

    Runtime: 2 ms (57.60%), Memory: 8 MB (4.58%) - Gitcode

---
+++ 278-first-bad-version/278. First Bad Version.cpp
@@ -0,0 +1,26 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+
+        int low = 1;
+        int high = n;
+        int ans = -99;
+
+        while(low <= high)
+        {
+            int mid = low + (high-low)/2;
+
+            if(isBadVersion(mid) == true)
+            {
+                ans = mid;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+
+        return ans;
+    }
+};

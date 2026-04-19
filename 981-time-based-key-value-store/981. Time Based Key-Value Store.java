f3d6ef9640589587fe38b6441b4ec07f75dbb6c7
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2026-04-19 13:32:00 +0530

    Runtime: 131 ms (85.08%), Memory: 108.2 MB (68.00%) - Gitcode

--- 981-time-based-key-value-store/981. Time Based Key-Value Store.java
+++ 981-time-based-key-value-store/981. Time Based Key-Value Store.java
@@ -49,9 +49,3 @@ class TimeMap {
     }
 }
 
-/**
- * Your TimeMap object will be instantiated and called as such:
- * TimeMap obj = new TimeMap();
- * obj.set(key,value,timestamp);
- * String param_2 = obj.get(key,timestamp);
- */

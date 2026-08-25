b99c6f8d465cf49346314873429b115bfb3e63e5
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2026-08-25 22:55:09 +0530

    Runtime: 29 ms (13.40%), Memory: 59.3 MB (5.44%) - Gitcode

---
+++ 930-binary-subarrays-with-sum/930. Binary Subarrays With Sum.java
@@ -0,0 +1,63 @@
+class Solution {
+
+    public int binarySearch(ArrayList<Integer> arr, Integer idx) {
+        int i = 0, j = arr.size() - 1;
+        int ans = Integer.MAX_VALUE;
+
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+
+            if (arr.get(mid) < idx) {
+                i = mid + 1;
+            } else {
+                ans = Math.min(ans, mid);
+                j = mid - 1;
+            }
+
+        }
+
+        if (ans == Integer.MAX_VALUE)
+            return -1;
+
+        return arr.size() - ans;
+    }
+
+    public int numSubarraysWithSum(int[] nums, int k) {
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        int curr = 0;
+        int ans = 0;
+        HashMap<Integer, ArrayList<Integer>> mpp = new HashMap<>();
+
+        int i = 0;
+        for (Integer it : nums) {
+            curr += it;
+            arr.add(curr);
+
+            if (mpp.containsKey(curr)) {
+                mpp.get(curr).add(i);
+            } else {
+                mpp.put(curr, new ArrayList<>());
+                mpp.get(curr).add(i);
+            }
+            i++;
+        }
+
+        for (i = 0; i < arr.size(); i++) {
+            int left = 0;
+            if (i > 0)
+                left = arr.get(i - 1);
+
+            int rem = k + left;
+
+            if (mpp.containsKey(rem)) {
+                int currAns = binarySearch(mpp.get(rem), i);
+                if (currAns != -1)
+                    ans += currAns;
+            }
+        }
+
+        return ans;
+
+    }
+}

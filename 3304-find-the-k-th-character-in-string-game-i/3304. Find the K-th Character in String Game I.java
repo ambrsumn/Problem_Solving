ccfc0e06b91c587c69efc5b380760eb484cab107
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2026-09-01 23:20:26 +0530

    Runtime: 15 ms (12.31%), Memory: 44.2 MB (30.65%) - Gitcode

---
+++ 3304-find-the-k-th-character-in-string-game-i/3304. Find the K-th Character in String Game I.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public char recur(StringBuilder s, int k)
+    {
+        IO.println(s);
+
+        if(s.length() > k)return s.charAt(k-1);
+
+        int n = s.length();
+
+        for(int i=0; i<n; i++)
+        {
+            int curr = ((int)s.charAt(i) - 97 + 1)%26 + 97;
+            s.append((char)curr);
+        }
+
+        return recur(s, k);
+    }
+
+    public char kthCharacter(int k) {
+        StringBuilder str = new StringBuilder("a");
+        return recur(str, k);
+    }
+}

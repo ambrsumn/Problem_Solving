db00f18fc80a30ca29dadd0e767257c8b14556df
Amber Suman <90973656+ambrsumn@users.noreply.github.com>
2026-09-22 22:28:47 +0530

    Runtime: 3 ms (59.52%), Memory: 42.7 MB (41.10%) - Gitcode

--- 1823-find-the-winner-of-the-circular-game/1823. Find the Winner of the Circular Game.java
+++ 1823-find-the-winner-of-the-circular-game/1823. Find the Winner of the Circular Game.java
@@ -1,27 +1,9 @@
 class Solution {
-
-    // public void recur(int curr, ArrayList<Integer> f, int k)
-    // {
-    //     if(f.size() == 1)return;
-
-    //     int n = f.size();
-    //     int eliminated = (f.get(curr) + k - 1)%f.size();
-
-    //     if(eliminated == 0)f.remove(f.size()-1);
-    //     else f.remove(eliminated-1);
-
-    //     f.remove(Math.abs(eliminated-1));
-    // }
     public int findTheWinner(int n, int k) {
 
         ArrayList<Integer> f = new ArrayList<>();
 
         for(int i=1; i<=n; i++)f.add(i);
-
-        // recur(0, f);
-
-        // return f.get(0);   
-
         int curr = 0;
 
         while(f.size() != 1)
